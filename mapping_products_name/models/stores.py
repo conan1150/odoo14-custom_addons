59109dca06d54db4870e00abc3bf8e985622da95
Intira-72 <intira.vinit@gmail.com>
2022-08-09 14:28:54 +0700

    module mapping_product_name : change db store_group to use res_partner

--- mapping_products_name/models/stores.py
+++ mapping_products_name/models/stores.py
@@ -7,14 +7,12 @@ class store_list(models.Model):
     _description = 'mapping_product_name.store_list'
 
     name = fields.Char('Store Name', required=True)
-    store_group_id = fields.Many2one('store.group', string='Group Name')
+    store_group_id = fields.Many2one('res.partner', string='Group')
+    
 
+class Partner(models.Model):
+    _inherit = 'res.partner'
 
-class store_group(models.Model):
-    _name = 'store.group'
-    _description = 'mapping_product_name.store_group'
-
-    name = fields.Char('Group Name')
-    store_lists = fields.One2many('store.list', 'store_group_id', string='Store Lists')
+    store_ids = fields.One2many('store.list', 'store_group_id', string='Store')
 
 
